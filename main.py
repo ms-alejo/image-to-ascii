868fb0589c4ae40ef6dac9027f63cf723c2ffd87
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-18 00:28:07 -0700

    added convert to ascii function but issue with orientation

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from PIL import Image
 
+# constants
+ASCII_CHARS = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 
-def get_pixel_matrix(image, height):
+
+def get_pixel_matrix(image, target_height):
     try:
-        image.thumbnail((height, 200))
+        image = image.transpose(method=Image.Transpose.TRANSVERSE)
+        if image is None:
+            raise ValueError("Image is not loaded properly.")
+
+        # resize
+        aspect_ratio = image.width / image.height
+        target_width = int(target_height * aspect_ratio)
+        image = image.resize((target_width, target_height))
+
         pixels = list(image.getdata())
         return [pixels[i : i + image.width] for i in range(0, len(pixels), image.width)]
 
@@ -11,7 +22,7 @@ def get_pixel_matrix(image, height):
         raise RuntimeError(f"Error generating pixel matrix: {e}")
 
 
-def convert_pixels(matrix):
+def convert_to_brightness(matrix):
     new_matrix = []
 
     for row in matrix:
@@ -19,6 +30,20 @@ def convert_pixels(matrix):
         for pixel in row:
             average = (pixel[0] + pixel[1] + pixel[2]) / 3
             new_matrix_row.append(average)
+
+        new_matrix.append(new_matrix_row)
+
+    return new_matrix
+
+
+def convert_to_ascii(matrix):
+    new_matrix = []
+
+    for row in matrix:
+        new_matrix_row = []
+        for pixel in row:
+            ascii_as_pixel = int((pixel / 255) * (len(ASCII_CHARS) - 1))
+            new_matrix_row.append(ASCII_CHARS[ascii_as_pixel])
 
         new_matrix.append(new_matrix_row)
 
@@ -33,14 +58,20 @@ try:
 
         # get pixel matrix
         pixel_matrix = get_pixel_matrix(img, 100)
-        print("Iterating through the pixels:")
-        for row in pixel_matrix:
-            print(row)
+        # print("Iterating through the pixels:")
+        # for row in pixel_matrix:
+        # print(row)
 
-        brightness_matrix = convert_pixels(pixel_matrix)
-        print("Iterating through converted pixels:")
-        for row in brightness_matrix:
-            print(row)
+        # get brightness matrix
+        brightness_matrix = convert_to_brightness(pixel_matrix)
+        # print("Iterating through converted pixels:")
+        # for row in brightness_matrix:
+        # print(row)
+
+        # get ascii matrix
+        ascii_matrix = convert_to_ascii(brightness_matrix)
+        for row in ascii_matrix:
+            print("".join(row))
 
 except FileNotFoundError:
     print("Error: Image file not found.")
