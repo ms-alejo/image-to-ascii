2dfc3c02f719c6244458680732c232e3f880d4f4
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-17 23:16:10 -0700

    refactored code and added extracting pixel data into a matrix

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from PIL import Image
 
-with Image.open("./asset/cat1.jpg") as img:
-    if img is None:
-        print("Error: failed loading image file")
-    else:
+
+def get_pixel_matrix(image, height):
+    try:
+        image.thumbnail((height, 200))
+        pixels = list(image.getdata())
+        return [pixels[i : i + image.width] for i in range(0, len(pixels), image.width)]
+    except Exception as e:
+        raise RuntimeError(f"Error generating pixel matrix: {e}")
+
+
+# load image
+try:
+    with Image.open("./asset/cat1.jpg") as img:
         print("Successfully loaded image!")
         print(f"Image size: {img.size[0]} x {img.size[1]}")
+
+        # get pixel matrix
+        pixel_matrix = get_pixel_matrix(img, 100)
+        print("Iterating through the pixels:")
+        for row in pixel_matrix:
+            print(row)
+
+except FileNotFoundError:
+    print("Error: Image file not found.")
+    exit()
+except Exception as e:
+    print(f"Error: Unable to open image. {e}")
+    exit()
