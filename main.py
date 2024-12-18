0d6afe7909c43008791b52a93389eeb1e8e62294
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-17 23:38:25 -0700

    added function for finding brightness matrix

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ def get_pixel_matrix(image, height):
         image.thumbnail((height, 200))
         pixels = list(image.getdata())
         return [pixels[i : i + image.width] for i in range(0, len(pixels), image.width)]
+
     except Exception as e:
         raise RuntimeError(f"Error generating pixel matrix: {e}")
+
+
+def convert_pixels(matrix):
+    new_matrix = []
+
+    for row in matrix:
+        new_matrix_row = []
+        for pixel in row:
+            average = (pixel[0] + pixel[1] + pixel[2]) / 3
+            new_matrix_row.append(average)
+
+        new_matrix.append(new_matrix_row)
+
+    return new_matrix
 
 
 # load image
@@ -20,6 +35,11 @@ try:
         pixel_matrix = get_pixel_matrix(img, 100)
         print("Iterating through the pixels:")
         for row in pixel_matrix:
+            print(row)
+
+        brightness_matrix = convert_pixels(pixel_matrix)
+        print("Iterating through converted pixels:")
+        for row in brightness_matrix:
             print(row)
 
 except FileNotFoundError:
